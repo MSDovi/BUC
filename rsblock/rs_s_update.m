cf78dc48d46188f2995d0c50e6dd8bf0e1fdbcb1
MSDovi <84228734+MSDovi@users.noreply.github.com>
2021-05-15 11:42:16 +0100

    Delete rsblock directory

--- rsblock/rs_s_update.m
+++
@@ -1,8 +0,0 @@
-function rs_block = rs_s_update(rs_block, new_s)
-%%Function Description
-% This function updates the s field of an s_block for a 'residual' state
-% variable. See RSBlock.m for documentation.
-
-rs_block.s = new_s;
-
-end
